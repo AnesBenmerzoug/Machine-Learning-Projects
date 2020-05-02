8250adc4025371629b56ba39654d5e999afb0dd6
Anes Benmerzoug <anes.benmerzoug@gmail.com>
2020-05-02 16:55:47 +0200

    Use a class to hold parameters instead of a yaml file

--- MNIST/main.py
+++ MNIST/main.py
@@ -1,9 +1,32 @@
-from collections import namedtuple
 import faulthandler
-import yaml
 import time
 
 from src import *
+
+
+class Parameters:
+    image_size = [28, 28]
+    hidden_size = 16
+    output_size = 10
+    batch_size = 32
+    savedModelDir = "./trained_models/"
+    testModelPath = "./trained_models/Trained_Model_99_Accuracy"
+    # Dataset Parameters
+    datasetDir = "./data/"
+    num_workers = 2
+    # Training Parameters
+    trainModel = False
+    useGPU = False
+    resumeTraining = False
+    num_epochs = 1000
+    max_norm = 400
+    # Optimizer Parameters
+    optimizer = "Adam"
+    learning_rate = 1.0e-2
+    momentum = 0.9
+    nesterov = True
+    update_frequency = 2
+
 
 if __name__ == "__main__":
     print("Starting time: {}".format(time.asctime()))
@@ -11,13 +34,9 @@ if __name__ == "__main__":
     # To have a more verbose output in case of an exception
     faulthandler.enable()
 
-    with open("parameters.yaml", "r") as params_file:
-        parameters = yaml.safe_load(params_file)
-        parameters = namedtuple("Parameters", (parameters.keys()))(*parameters.values())
-
-    if parameters.trainModel is True:
+    if Parameters.trainModel is True:
         # Instantiating the trainer
-        trainer = MNISTTrainer(parameters)
+        trainer = MNISTTrainer(Parameters)
         # Training the model
         avg_losses = trainer.train_model()
         # Plot losses
@@ -30,7 +49,7 @@ if __name__ == "__main__":
 
     else:
         # Instantiating the test
-        tester = MNISTTester(parameters)
+        tester = MNISTTester(Parameters)
         # Testing the model
         tester.test_random_sample()
         # Testing the model accuracy
